8923707f87a623aa0ac618d9da10bb4a74d48789
Nate Harada <nharada1@gmail.com>
2013-11-09 17:42:26 -0500

    More changes, trying to use node.js

--- js/lightshow.js
+++ js/lightshow.js
@@ -1,3 +1,18 @@
+function init() { 
+	var PORT = 33333;
+	var HOST = '127.0.0.1';
+
+	var dgram = require('dgram');
+	var message = new Buffer('My KungFu is Good!');
+
+	var client = dgram.createSocket('udp4');
+	client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
+		if (err) throw err;
+		console.log('UDP message sent to ' + HOST +':'+ PORT);
+		client.close();
+	});
+}
+
 require(['$api/audio', '$api/models'], function(audio, models) {
   var bands    = audio.BAND10;
   var numBands = bands.length;
@@ -6,10 +21,13 @@ require(['$api/audio', '$api/models'], function(audio, models) {
   console.log(audio.BAND10);
   analyzer.addEventListener('audio', function(evt) {
 	if (evt.audio.spectrum.left[1] > 0) {
-		document.getElementById("output").innerHTML="BOOM";
+		websocket.send("Test");
+		document.getElementById("output").innerHTML="";
 	}
 	else {
 		document.getElementById("output").innerHTML="";
 	}
   });
 });
+
+window.addEventListener("load", init, false);
