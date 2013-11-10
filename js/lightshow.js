704c54572a3ebb7466a77c6384380b1c1ae56f25
Nate Harada <nharada1@gmail.com>
2013-11-10 12:21:57 -0500

    Changes to add timing to spotify app

--- js/lightshow.js
+++ js/lightshow.js
@@ -1,19 +1,31 @@
 require(['$api/audio', '$api/models'], function(audio, models) {
   var bands    = audio.BAND10;
   var numBands = bands.length;
-  var analyzer = audio.RealtimeAnalyzer.forPlayer(models.player, bands);
+  var analyzer = audio.BufferAnalyzer.forPlayer(models.player, bands);
   
   var socket = io.connect('http://localhost:3001'); 
+  var startTime = null;
   
   document.getElementById("output").innerHTML = "";
   console.log(audio.BAND10);
   analyzer.addEventListener('audio', function(evt) {
-	if (evt.audio.spectrum.left[1] > 0) {
+	if (evt.audio.spectrum.left[1] > 0 && evt.audio.spectrum.right[1] > 0) {
 		socket.emit('TEST');
 		document.getElementById("output").innerHTML="BOOM";
 	}
 	else {
-		document.getElementById("output").innerHTML="";
+		if (startTime) {
+			var deltaSecs = (evt.deadline.getTime() - startTime.getTime()) / 1000;
+			document.getElementById("output").innerHTML=deltaSecs;
+		}
 	}
+  });
+  
+  models.player.addEventListener('change', function(evt) {
+	models.player.load('playing').done(function(track) {
+		if (models.player.playing) {
+			startTime = new Date();
+		}
+	});
   });
 });
