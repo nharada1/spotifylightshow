335349e2aa9dd08a01a550dbd14221d167d9f038
Nate Harada <nharada1@gmail.com>
2013-11-10 13:43:09 -0500

    More drop changes

--- js/lightshow.js
+++ js/lightshow.js
@@ -1,3 +1,9 @@
+const DROP = 0;
+const VERSE = 1;
+
+var listODrops = null;
+var currentState = VERSE;
+  
 require(['$api/audio', '$api/models'], function(audio, models) {
   var bands    = audio.BAND10;
   var numBands = bands.length;
@@ -5,22 +11,47 @@ require(['$api/audio', '$api/models'], function(audio, models) {
   
   var socket = io.connect('http://localhost:3001'); 
   var startTime = null;
-  var listODrops = null;
+  var deltaSecs = 0;
   
-  document.getElementById("output").innerHTML = "";
+  document.getElementById("boom").innerHTML = "";
   console.log(audio.BAND10);
+  
+  getAudioAnalysis('scary', 'skrillex');
+  
   analyzer.addEventListener('audio', function(evt) {
 
 	if (evt.audio.spectrum.left[1] > 0 && evt.audio.spectrum.right[1] > 0) {
-		socket.emit('TEST');
-		document.getElementById("output").innerHTML="BOOM";
+		if (currentState == DROP) {
+			socket.emit('DROP');
+		} else {
+			socket.emit('VERSE');
+		}
+		document.getElementById("boom").innerHTML="BOOM";
 	}
 	else {
-		if (startTime) {
-			var deltaSecs = (evt.deadline.getTime() - startTime.getTime()) / 1000;
-			document.getElementById("output").innerHTML=deltaSecs;
+		if (currentState == DROP) {
+			stateString = "Drop";
+		} else {
+			stateString = "Verse";
+		}
+		document.getElementById("state").innerHTML=stateString;
+		document.getElementById("boom").innerHTML="";
+	}
+	
+	if (startTime) {
+		var deltaSecs = (evt.deadline.getTime() - startTime.getTime()) / 1000;
+		document.getElementById("deadline").innerHTML= deltaSecs;
+		if (listODrops) {
+			for (var i=0; i<listODrops.length; i++) {
+				if (deltaSecs > listODrops[i].time) {
+					console.log(listODrops[i].time);
+					if (listODrops[i].type == "drop") currentState = DROP;
+					if (listODrops[i].type == "verse") currentState = VERSE;
+				}
+			}
 		}
 	}
+	
   });
   models.player.addEventListener('change', function(evt) {
 	models.player.load('playing').done(function(track) {
@@ -29,9 +60,9 @@ require(['$api/audio', '$api/models'], function(audio, models) {
 		}
 	});
   });
-  getAudioAnalysis('scary', 'skrillex');
 });
 
 function dropList(drops){
- console.log(drops);
+	listODrops = drops;
+	console.log(drops);
 }
