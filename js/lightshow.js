b2c6d475b482d2bb6c9d475e54084c10b40d2fc9
Jasper Degens <jasperdegens@gmail.com>
2013-11-09 22:19:55 -0500

    no changes

--- js/lightshow.js
+++ js/lightshow.js
@@ -1,16 +1,9 @@
 function init() { 
-	var PORT = 33333;
-	var HOST = '127.0.0.1';
-
-	var dgram = require('dgram');
-	var message = new Buffer('My KungFu is Good!');
-
-	var client = dgram.createSocket('udp4');
-	client.send(message, 0, message.length, PORT, HOST, function(err, bytes) {
-		if (err) throw err;
-		console.log('UDP message sent to ' + HOST +':'+ PORT);
-		client.close();
-	});
+ var socket = io.connect('http://localhost');
+  socket.on('news', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+  });
 }
 
 require(['$api/audio', '$api/models'], function(audio, models) {
